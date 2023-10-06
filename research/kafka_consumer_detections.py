4379fc88c8107d8292060a28878ea3db03ffb0c1
stanislav.fateev <fateevsstanislav@gmail.com>
2023-10-06 21:10:55 +0300

    Add kafka consumer to parse detection.

---
+++ research/kafka_consumer_detections.py
@@ -0,0 +1,33 @@
+import json
+from pprint import pprint
+
+from confluent_kafka import Consumer
+
+from constants import TOPIC, TOPIC2
+
+
+def main():
+    consumer = Consumer({'bootstrap.servers': 'localhost:9093',
+                         'group.id': 'mygroup'})
+    consumer.subscribe([TOPIC2])
+    while True:
+        msg = consumer.poll(1.0)
+
+        if msg is None:
+            continue
+        if msg.error():
+            print("Consumer error: {}".format(msg.error()))
+            continue
+
+        text = msg.value().decode('utf-8')
+        print(type(text), text)
+        try:
+            results = json.loads(text)
+            print('Received detections:')
+            pprint(results)
+        except Exception as e:
+            print(e)
+
+
+if __name__ == '__main__':
+    main()
