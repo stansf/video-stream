843115967669f3ff915355e0168cde3814e5b20d
stanislav.fateev <fateevsstanislav@gmail.com>
2023-10-06 19:53:54 +0300

    Producer and consumer for images.

---
+++ research/kafka_consumer_img.py
@@ -0,0 +1,36 @@
+from confluent_kafka import Consumer
+import cv2
+import numpy as np
+import io
+from constants import TOPIC, TOPIC2
+import sys
+
+def main():
+    consumer = Consumer({'bootstrap.servers': 'localhost:9093',
+                         'group.id': 'mygroup',
+                         'auto.offset.reset': 'latest',
+                         })
+    consumer.subscribe([TOPIC2])
+    i = 0
+    while True:
+        msg = consumer.poll(1.0)
+
+        if msg is None:
+            continue
+        if msg.error():
+            print("Consumer error: {}".format(msg.error()))
+            continue
+
+        print('Received message.')
+        im_bytes = msg.value()
+        print(sys.getsizeof(im_bytes))
+        print(im_bytes.decode('utf-8'))
+        image_file_bytes = np.asarray(bytearray(io.BytesIO(im_bytes).read()),
+                                      dtype=np.uint8)
+        img = cv2.imdecode(image_file_bytes, cv2.IMREAD_COLOR)
+        cv2.imwrite(f'image_get_{i}.png', img)
+        i += 1
+
+
+if __name__ == '__main__':
+    main()
---
+++ research/kafka_producer_img.py
@@ -0,0 +1,55 @@
+from time import sleep
+
+import cv2
+from confluent_kafka import Producer
+
+from constants import TOPIC
+
+
+def main0():
+    producer = Producer({'bootstrap.servers': 'localhost:9093'})
+    for i in range(10):
+        producer.produce(TOPIC, b'Hello')
+        print('Send', i+1)
+        sleep(0.8)
+
+
+def delivery_report(err, msg):
+    """ Called once for each message produced to indicate delivery result.
+        Triggered by poll() or flush(). """
+    if err is not None:
+        print('Message delivery failed: {}'.format(err))
+    else:
+        print('Message delivered to {} [{}]'.format(msg.topic(),
+                                                    msg.partition()))
+
+
+def main():
+    p = Producer({'bootstrap.servers': 'localhost:9093'})
+
+    i = 0
+    while True:
+        img = cv2.imread('image.png')
+        img_enc = cv2.imencode('.png', img)
+        # print(img_enc[1].shape)
+        # print(img_enc[1].tobytes())
+        img_b = img_enc[1].tobytes()
+        print(type(img_b), len(img_b))
+        # data = f'Hello {i}'
+        # Trigger any available delivery report callbacks from previous produce() calls
+        p.poll(0.5)
+
+        # Asynchronously produce a message. The delivery report callback will
+        # be triggered from the call to poll() above, or flush() below, when the
+        # message has been successfully delivered or failed permanently.
+        p.produce(TOPIC, img_b, callback=delivery_report)
+        sleep(5)
+        i += 1
+
+    # Wait for any outstanding messages to be delivered and delivery report
+    # callbacks to be triggered.
+    p.flush()
+
+
+if __name__ == '__main__':
+    main()
